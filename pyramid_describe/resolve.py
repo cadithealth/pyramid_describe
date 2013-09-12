14256fc38d13ef8dc21e7fad6351a8593be5357b
grabner <pjg.github@ubergrabner.net>
2013-09-12 13:55:25 -0400

    relative import

--- pyramid_describe/resolve.py
+++ pyramid_describe/resolve.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# auth: Philip J Grabner <grabner@cadit.com>
+# date: 2013/09/09
+# copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
+#------------------------------------------------------------------------------
 
-from isstr import isstr
+from .isstr import isstr
 
 #------------------------------------------------------------------------------
 def resolve(spec):
