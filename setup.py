b3385f396fa12150efb9f2116ba5d62a9294172f
grabner <pjg.github@ubergrabner.net>
2013-09-19 18:00:57 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ classifiers = [
 
 setup(
   name                  = 'pyramid_describe',
-  version               = '0.1.20',
+  version               = '0.1.21',
   description           = 'A pyramid plugin that describes a pyramid application URL hierarchy via inspection.',
   long_description      = read('README.rst'),
   classifiers           = classifiers,
