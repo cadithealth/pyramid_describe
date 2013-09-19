0defccf6a4e8d4f3772ec8342f2e8733b271a56a
grabner <pjg.github@ubergrabner.net>
2013-09-19 14:34:03 -0400

    added helpers "runFilters" and "tolist"

--- pyramid_describe/util.py
+++ pyramid_describe/util.py
@@ -6,10 +6,34 @@
 # copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
 #------------------------------------------------------------------------------
 
+from pyramid.settings import aslist
+from pyramid_controllers.util import adict, pick
+
 from .isstr import isstr
 from .resolve import resolve
 from .reparse import reparse
-from pyramid_controllers.util import adict, pick
+
+#------------------------------------------------------------------------------
+def runFilters(filters, target, *args, **kw):
+  if not target:
+    return None
+  if not filters:
+    return target
+  for filt in filters:
+    target = filt(target, *args, **kw)
+    if not target:
+      break
+  return target
+
+#------------------------------------------------------------------------------
+def tolist(obj):
+  if not obj:
+    return []
+  if isstr(obj):
+    return aslist(obj)
+  if isinstance(obj, (list, tuple)):
+    return obj
+  return [obj]
 
 #------------------------------------------------------------------------------
 # end of $Id$
