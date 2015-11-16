f947847d717e94db22793092a3eb9a2b705e680c
grabner <pjg.github@ubergrabner.net>
2015-11-16 17:15:16 -0500

    added plugin loading unit test

--- pyramid_describe/test_helpers.py
+++ pyramid_describe/test_helpers.py
@@ -6,8 +6,11 @@
 # copy: (C) Copyright 2014-EOT Cadit Inc., All Rights Reserved.
 #------------------------------------------------------------------------------
 
+import unittest
+
 import pkg_resources
 import pyramid_controllers.test_helpers
+import asset
 
 #------------------------------------------------------------------------------
 class TestHelper(pyramid_controllers.test_helpers.TestHelper):
@@ -22,6 +25,33 @@ class TestHelper(pyramid_controllers.test_helpers.TestHelper):
       for key, val in symbols.items():
         data = data.replace('{{' + key + '}}', val)
     return data
+
+
+#------------------------------------------------------------------------------
+def plugin_test_symbol(): pass
+plugin_test_symbol.after = 'numpydoc'
+plugin_test_symbol.before = 'docorator'
+
+#------------------------------------------------------------------------------
+class TestPlugins(unittest.TestCase):
+
+  #----------------------------------------------------------------------------
+  def test_parsers_standard(self):
+    self.assertEqual(
+      [p.name for p in asset.plugins('pyramid_describe.plugins.entries.parsers')],
+      ['docref', 'title', 'numpydoc', 'docorator'])
+
+  #----------------------------------------------------------------------------
+  def test_parsers_extra_asset(self):
+    self.assertEqual(
+      [p.name for p in asset.plugins(
+        'pyramid_describe.plugins.entries.parsers',
+        '+pyramid_describe.test_helpers.plugin_test_symbol',
+      )],
+      ['docref', 'title', 'numpydoc',
+       'pyramid_describe.test_helpers.plugin_test_symbol',
+       'docorator'])
+
 
 #------------------------------------------------------------------------------
 # end of $Id$
