5eafcaf86c57ca68a8ffeda18a9fb004b0f4ebe8
grabner <pjg.github@ubergrabner.net>
2016-04-21 17:42:54 -0400

    added JSON partial parsing utility (that is both 2.7.3 and 2.7.6 compatible)

--- pyramid_describe/util.py
+++ pyramid_describe/util.py
@@ -6,7 +6,10 @@
 # copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
 #------------------------------------------------------------------------------
 
-import re, binascii
+import re
+import json
+import binascii
+
 from pyramid.settings import aslist, asbool, truthy
 from pyramid_controllers.util import adict, pick
 
@@ -55,6 +58,31 @@ def tolist(obj):
 #------------------------------------------------------------------------------
 def tag(text):
   return binascii.hexlify(text).lower()
+
+#------------------------------------------------------------------------------
+def jsonParse(text, partial=False):
+  '''
+  JSON-decodes `text` and returns the result. If `partial` is truthy,
+  then JSON-decodes as much of `text` as possible (at least *some*),
+  and returns a tuple of (result, remainder-text).
+  '''
+  if not partial:
+    return json.loads(text)
+  try:
+    return ( json.loads(text), '' )
+  except ValueError as exc:
+    if not str(exc).startswith('Extra data: line 1 column '):
+      raise
+    # NOTE: first trying idx as-is, then idx-1 because json changed
+    #       from being 0-based to 1-based from python 2.7.3 to 2.7.6.
+    idx = int(str(exc).split()[5])
+    try:
+      return ( json.loads(text[:idx]), text[idx:] )
+    except ValueError as exc:
+      if not str(exc).startswith('Extra data: line 1 column '):
+        raise
+    idx = idx - 1
+    return ( json.loads(text[:idx]), text[idx:] )
 
 #------------------------------------------------------------------------------
 # end of $Id$
